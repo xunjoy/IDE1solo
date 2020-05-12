a7331f08d5f8ba62f641dc9dfda177816465a1d5
Joy Zhang <vjxun@hotmail.com>
2020-05-12 17:11:34 +0100

    background is always red now, must fix

--- sidebar.js
+++ sidebar.js
@@ -1,14 +1,26 @@
-/* adding rearrange overlay */
-<style>
-.rearrange-popup{
-        position: fixed;
-        top: 0px;
-        bottom: 0px;
-        left: 0px;
-        right: 0px;
-        z-index: 999999999;
-        /* Setting background a diff color for testing */
-        background: #ddbc75;
-     } 
-</style>
-    
+
+chrome.runtime.onConnect.addListener(function(devToolsConnection) {
+    //alert("inside background.js -- onConnect");
+      // assign the listener function to a variable so we can remove it later
+      var devToolsListener = function(message, sender, sendResponse) {
+        //alert("injecting Content Script");
+          // Inject a content script into the identified tab
+          if (message.name == "init") {
+              
+            //alert("injecting Content Script - inside init");
+              //alert("tab ID " + message.tabId) ;
+              //alert("message.scriptToInject: " + message.scriptToInject) ;
+              
+              
+              chrome.tabs.executeScript(message.tabId,
+              {file: message.scriptToInject });
+          }
+  
+      }
+      // add the listener
+      devToolsConnection.onMessage.addListener(devToolsListener);
+  
+      devToolsConnection.onDisconnect.addListener(function() {
+           devToolsConnection.onMessage.removeListener(devToolsListener);
+      });
+  })
