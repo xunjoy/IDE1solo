6c8bd023d20e7482596f11d58767051bf007c046
Joy Zhang <vjxun@hotmail.com>
2020-05-15 18:33:30 +0100

    added input bar at header of popup

--- visualize.js
+++ visualize.js
@@ -2,7 +2,7 @@ var theButton = document.getElementById("d3js");
 
 theButton.addEventListener("click",function(tab) {
     // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
+    console.log('Visualizing the data on ' + tab.url + ' !');
     chrome.tabs.executeScript({
         code: 'document.body.style.backgroundColor="blue"',
         //file: "d3/d3.min.js",
