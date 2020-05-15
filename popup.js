97d5576f917f06da382229b4e4b9d4d73a396f0f
Joy Zhang <vjxun@hotmail.com>
2020-05-15 19:46:23 +0100

    added reactive mood tracker but disappears every time popup closes

--- popup.js
+++ popup.js
@@ -1,5 +1,34 @@
 $(function(){
     $('#feeling').keyup(function(){
-        $('#greet').text('I am feeling ' + $('#feeling').val());
+        $('#greet').text('I am feeling ' + $('#feeling').val() +  ' out of 10');
+    });
+
+    $('#new-feeling').click(function(){
+        $('#greet').text('Feeling submitted');
+
+        $('#greet2').text('Last time  you  were feeling ' + $('#feeling').val() + ' out of 10');
+
+        chrome.storage.sync.get('mood', function(avgMood){
+            
+            var newMood = 0;
+
+            if (avgMood.mood){
+                newMood += parseInt(avgMood.mood);
+            }
+
+            var feelingNum = $('#feeling').val();
+
+            if ('#new-feeling'){
+                //newMood += parseInt(feelingNum);
+                newMood = parseInt(feelingNum);
+            }
+
+            chrome.storage.sync.set({'mood' : newMood});
+
+            
+
+            $('#feeling').val('');
+        })
+
     })
-})
+});
