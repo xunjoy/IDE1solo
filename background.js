02b66fac4d1e81749b910abd49ef8a37eabd06da
Joy Zhang <vjxun@hotmail.com>
2020-05-16 07:31:09 +0100

    background.js is running

--- background.js
+++ background.js
@@ -1,14 +1,51 @@
-var toggle = false;
+// var toggle = false;
 
-function toggleSwitch() {
-  toggle = !toggle;
-  if (!toggle) { chrome.browserAction.setIcon({ path: "icon_newnews_active.png" }); }
-  else { chrome.browserAction.setIcon({ path: "icon_newnews.png" }); }
-}
+// function toggleSwitch() {
+//   toggle = !toggle;
+//   if (!toggle) { chrome.browserAction.setIcon({ path: "icon_newnews_active.png" }); }
+//   else { chrome.browserAction.setIcon({ path: "icon_newnews.png" }); }
+// }
 
-chrome.browserAction.onClicked.addListener(function (tab) { toggleSwitch(); });
+// chrome.browserAction.onClicked.addListener(function (tab) { toggleSwitch(); });
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.greeting == "isActive") { sendResponse({ isToggled: toggle }); }
-  });
+// chrome.runtime.onMessage.addListener(
+//   function (request, sender, sendResponse) {
+//     if (request.greeting == "isActive") { sendResponse({ isToggled: toggle }); }
+//   });
+
+
+
+//testing get url
+
+// chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+//   let url = tabs[0].url;
+//   console.log(tabs[0].url);
+//   // use `url` here inside the callback because it's asynchronous!
+
+
+//   if (tabs[0].url == "bbc.co.uk"){
+//     //alert(test);
+
+//     var notif = {
+//       type: 'basic',
+//       iconUrl: 'icon_newnews_active.png',
+//       title: 'This is a news platform',
+//       message: 'To prevent information overload, it’s important to be mindful of your mental health and news consumption.'
+//   }
+  
+//   chrome.notifications.create('bbcNotif', notif);
+//   };
+
+// });
+console.log('background.js is running');
+
+  var notif = {
+    type: 'basic',
+    iconUrl: 'icon_newnews_active.png',
+    title: 'This is a news platform',
+    message: 'To prevent information overload, it’s important to be mindful of your mental health and news consumption.'
+  }
+  
+  chrome.notifications.create('bbcNotif', notif,function(){});
+
+
